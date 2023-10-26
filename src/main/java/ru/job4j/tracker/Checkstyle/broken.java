8062477a09a6d5bb35243f228868aa6ced1d3631
q100vmike <melkin@mail.ru>
2023-10-26 12:23:03 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/tracker/Checkstyle/broken.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker.checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+
+    public String surname;
+
+    public static final String NEWVALUE = "";
+
+    void echo() { }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int b, int c, int d, int e, int f, int g, int h) {
+
+    }
+
+    String name;
+
+    Broken() {
+
+    }
+
+}
