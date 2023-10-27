7620aa7272c6796ada8338a0cc0c04bb6eb3d3ae
q100vmike <melkin@mail.ru>
2023-10-27 12:24:14 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void show() {
+        System.out.println("active= " + this.active);
+        System.out.println("status= " + this.status);
+        System.out.println("message= " + this.message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error errorOne = new Error(true, 2, "errorOne");
+        Error errorTwo = new Error(false, 100, "errorTwo");
+        Error errorThree = new Error(true, 777, "errorThree");
+
+        errorOne.show();
+        errorTwo.show();
+        errorThree.show();
+        error.show();
+    }
+}
