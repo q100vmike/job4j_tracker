984b4fba5db7040658f16d35e02bb86ae4f37048
q100vmike <melkin@mail.ru>
2023-10-28 23:08:41 +0300

    2.1. Рефакторинг - Площадь треугольника. [#122642]

---
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.oop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class TriangleTest {
+    @Test
+    void calcAreaTriangle24_5() {
+        double expected = 24.5;
+        Point a = new Point(0, 10);
+        Point b = new Point(1, 1);
+        Point c = new Point(6, 5);
+        Triangle tr = new Triangle(a, b, c);
+        double out = tr.area(a, b, c);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void calcAreaTriangleNotValid() {
+        double expected = -1;
+        Point a = new Point(0, 10);
+        Point b = new Point(0, 0);
+        Point c = new Point(0, 5);
+        Triangle tr = new Triangle(a, b, c);
+        double out = tr.area(a, b, c);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
