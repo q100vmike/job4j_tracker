50cef7937edfbb671452c2ae0c58ed9ec442f662
q100vmike <melkin@mail.ru>
2023-11-02 15:29:22 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,21 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        int i = 0;
+        Item[] resitems = new Item[size];
+        for (int index = 0; index < size; index++) {
+            if (items[index].getName() == key) {
+                resitems[i] = items[index];
+                i++;
+            }
+        }
+        return Arrays.copyOf(resitems, i);
     }
 }
