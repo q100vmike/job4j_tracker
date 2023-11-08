67970919c495cc13ad0c9647f660664b50570320
q100vmike <melkin@mail.ru>
2023-11-08 17:39:23 +0300

    11.1. Реализация меню за счет шаблона стратегия. [#181780]

---
+++ src/main/java/ru/job4j/tracker/FindAllAction.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker;
+
+public class FindAllAction implements UserAction {
+    @Override
+    public String name() {
+        return "Показать все заявки";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        Item[] items = tracker.findAll();
+        if (items.length > 0) {
+            for (Item item : items) {
+                System.out.println(item);
+            }
+        } else {
+            System.out.println("Хранилище еще не содержит заявок");
+        }
+        return true;
+    }
+}
