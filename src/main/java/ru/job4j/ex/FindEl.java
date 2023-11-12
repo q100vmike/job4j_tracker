0546342f9d14bdfdb4f8d1d6f663acdc17b7e44e
q100vmike <melkin@mail.ru>
2023-11-12 14:28:10 +0300

    0.4. Пользовательские исключения. [#219367 #459117]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+import java.util.Objects;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (Objects.equals(value[i], key)) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Элемент не найден");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] array = new String[] {"one", "two", "three", "four"};
+            FindEl.indexOf(array, "kod");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
