a22232fdaf9efcd5abe2ecc73eecb418e1aa00ec
q100vmike <melkin@mail.ru>
2024-01-14 15:14:38 +0300

    3. Компаратор для строк [#35008]

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        //int i = left.length() >= right.length() ? right.length() : left.length();
+        int result = 0;
+
+        for (int j = 0; j < left.length(); j++) {
+            if (Character.isDigit(left.charAt(j)) && Character.isDigit(right.charAt(j))) {
+                result = Integer.compare(left.charAt(j), right.charAt(j));
+            } else {
+                result = Character.compare(left.charAt(j), right.charAt(j));
+            }
+            if (result != 0) {
+                break;
+            }
+        }
+        if (result == 0 && left.length() != right.length()) {
+            result = Integer.compare(left.length(), right.length());
+        }
+        return result;
+    }
+}
