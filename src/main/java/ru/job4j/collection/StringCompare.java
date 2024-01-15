113678aa5496d888e99e87f8080e16aa413790f7
q100vmike <melkin@mail.ru>
2024-01-15 16:35:36 +0300

    3. Компаратор для строк [#35008]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -7,19 +7,11 @@ public class StringCompare implements Comparator<String> {
     public int compare(String left, String right) {
         int result = 0;
 
-        for (int j = 0; j < left.length(); j++) {
-            if (Character.isDigit(left.charAt(j)) && Character.isDigit(right.charAt(j))) {
-                result = Integer.compare(left.charAt(j), right.charAt(j));
-            } else {
-                result = Character.compare(left.charAt(j), right.charAt(j));
-            }
-            if (result != 0) {
-                break;
+        for (int j = 0; j < Math.min(left.length(), right.length()); j++) {
+            if (left.charAt(j) != right.charAt(j)) {
+                return Character.compare(left.charAt(j), right.charAt(j));
             }
         }
-        if (result == 0 && left.length() != right.length()) {
-            result = Integer.compare(left.length(), right.length());
-        }
-        return result;
+            return Integer.compare(left.length(), right.length());
     }
 }
