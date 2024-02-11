7a58ca29accd031e0290efe35b4f76a4356751cf
q100vmike <melkin@mail.ru>
2024-02-11 13:20:30 +0300

    6. Список адресов [#110062 #480280]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -8,7 +8,7 @@ public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(profile -> profile.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 }
