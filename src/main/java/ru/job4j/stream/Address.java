7cf97f803ac6fbd32d9fcc47d83ebda6a78ac539
q100vmike <melkin@mail.ru>
2024-02-11 13:45:14 +0300

    7. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Address.java
+++ src/main/java/ru/job4j/stream/Address.java
@@ -3,6 +3,7 @@ package ru.job4j.stream;
 import java.util.Objects;
 
 public class Address {
+
     private String city;
 
     private String street;
@@ -16,6 +17,10 @@ public class Address {
         this.street = street;
         this.home = home;
         this.apartment = apartment;
+    }
+
+    public String getCity() {
+        return city;
     }
 
     @Override
