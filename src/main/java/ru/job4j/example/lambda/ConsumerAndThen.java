9998495f580587617119a026aee9ee2dc21b0bd9
q100vmike <melkin@mail.ru>
2024-02-22 16:18:59 +0300

    tests

---
+++ src/main/java/ru/job4j/example/lambda/ConsumerAndThen.java
@@ -0,0 +1,12 @@
+package ru.job4j.example.lambda;
+
+
+import java.util.function.Consumer;
+
+public class ConsumerAndThen {
+    public static Consumer<String> consumer(String input) {
+         Consumer<String> print = (str) -> System.out.print(str);
+         Consumer<String> ln = (str) -> System.out.println("");
+         return print.andThen(ln);
+    }
+}
