55c3f71f59b8ddc29fbfbc166b827dc67d0787a8
q100vmike <melkin@mail.ru>
2024-02-26 16:57:56 +0300

    tests

---
+++ src/main/java/ru/job4j/example/stream/CollectorArithmetic.java
@@ -0,0 +1,29 @@
+package ru.job4j.example.stream;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class CollectorArithmetic {
+    public static Integer collect(List<Integer> list) {
+        Supplier<List<Integer>> supplier = LinkedList::new;
+        BiConsumer<List<Integer>, Integer> consumer = List::add;
+        BinaryOperator<List<Integer>> merger = (xs, ys) -> {
+            xs.addAll(ys);
+            return xs;
+        };
+        Function<List<Integer>, Integer> function = (ns) -> {
+            Integer rls = 1;
+            for (Integer n : ns) {
+                rls *= n;
+            }
+            return rls;
+        };
+        return list.stream()
+                .collect(Collector.of(supplier, consumer, merger, function));
+    }
+}
