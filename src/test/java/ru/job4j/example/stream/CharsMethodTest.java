a8006676c6de50a76786ecbde06a7db37e0fb8d1
q100vmike <melkin@mail.ru>
2024-03-01 12:14:37 +0300

    tests

---
+++ src/test/java/ru/job4j/example/stream/CharsMethodTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.example.stream;
+
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class CharsMethodTest {
+
+    @Test
+    public void test() {
+        String input = "123";
+        List<Character> expect = List.of('1', '2', '3');
+        assertEquals(expect, CharsMethod.symbols(input));
+    }
+
+}
