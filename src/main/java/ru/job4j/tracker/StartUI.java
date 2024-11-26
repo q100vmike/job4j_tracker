6a80a6e42aad89d903be7d2cb490d1844b46303a
q100vmike <melkin@mail.ru>
2024-11-26 17:41:04 +0300

    1. Профилирование. [#1591]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -38,7 +38,7 @@ public class StartUI {
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
-        Store tracker = new SqlTracker();
+        Store tracker = new MemTracker();
         List<UserAction> actions = Arrays.asList(
             new Create(output),
             new FindAll(output),
